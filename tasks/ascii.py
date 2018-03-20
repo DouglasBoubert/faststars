c766c24e96b9260b3ab368a376ee7359e1a13edc
Douglas Boubert <dpb33@cam.ac.uk>
2018-03-20 14:22:18 +0000

    Added HVS Survey II to ascii

--- tasks/ascii.py
+++ tasks/ascii.py
@@ -20,28 +20,38 @@ from astropy.io.ascii import read
 from astropy.time import Time as astrotime
 
 from ..supernova import SUPERNOVA
+from .utils import rgc_to_dhel
 
 
 def do_ascii(catalog):
     """Process ASCII files extracted from datatables of published works."""
     task_str = catalog.get_current_task_str()
 
-    # 2017ApJ...836...60L
+    # 2007ApJ...660..311B
     datafile = os.path.join(catalog.get_current_task_repo(), 'ASCII',
-                            '2017ApJ...836...60L-tab1.cds')
+                            'ApJ_660_311_table1.csv')
     data = read(datafile, format='cds')
     for row in pbar(data, task_str):
-        oname = row['ID']
-        name, source = catalog.new_entry(oname, bibcode='2017ApJ...836...60L')
-        photodict = {
-            PHOTOMETRY.TIME: str(row['MJD']),
-            PHOTOMETRY.BAND: row['Filter'],
-            PHOTOMETRY.U_TIME: 'MJD',
-            PHOTOMETRY.MAGNITUDE: row['mag'],
-            PHOTOMETRY.E_MAGNITUDE: row['e_mag'],
-            PHOTOMETRY.SOURCE: source
-        }
-        catalog.entries[name].add_photometry(**photodict)
+        oname = 'SDSS'+str(row['SDSS'])[1:]
+        name, source = catalog.new_entry(oname, bibcode='2007ApJ...660..311B')
+        gallon = float(str(row['Glon']))
+        gallat = float(str(row['Glat']))
+        ra, dec = coord(
+            l=gallon * u.degree, b=gallat * u.degree,
+            frame='galactic').icrs.to_string(
+                'hmsdms', sep=':').split()
+        catalog.entries[name].add_quantity(
+            FASTSTARS.RA, ra, source=source)
+        catalog.entries[name].add_quantity(
+            FASTSTARS.DEC, dec, source=source)
+        catalog.entries[name].add_quantity(
+            FASTSTARS.VELOCITY, float(str(row['Vhelio'])), source=source)
+        galrad_MS = float(str(row['Ra']))
+        galrad_BHB = float(str(row['Rb']))
+        dhel_MS = rgc_to_dhel(galrad_MS,gallon,gallat)
+        dhel_BHB = rgc_to_dhel(galrad_BHB,gallon,gallat)
+        catalog.entries[name].add_quantity(
+            FASTSTARS.LUMDIST, dhel_MS, lower_limit=dhel_BHB, source=source)
     catalog.journal_entries()
 
     return
