27e635a0b5ea6e14166c31b6855f45f6076411f4
Douglas Boubert <dpb33@cam.ac.uk>
2018-03-21 17:34:52 +0000

    Added Theissen 2014

--- tasks/ascii.py
+++ tasks/ascii.py
@@ -281,5 +281,35 @@ def do_ascii(catalog):
         catalog.entries[name].add_quantity(
                 FASTSTARS.SPECTRAL_TYPE, str(row['Type']), source=source)
     catalog.journal_entries()
+    
+    # 2014ApJ...794..146T
+    datafile = os.path.join(catalog.get_current_task_repo(), 'ASCII',
+                            'ApJ794146.csv')
+    data = read(datafile, format='csv')
+    for row in pbar(data, task_str):
+        oname = str(row['Catalog']).replace(' ','')
+        name, source = catalog.new_entry(oname, bibcode='2014ApJ...794..146T')
+        radec = oname.strip('SDSSJ')
+        radec = radec[0:2]+' '+radec[2:4]+' '+radec[4:9]+' '+radec[9:12]+' '+radec[12:14]+' '+radec[14:18]
+        ra, dec = coord(radec, 
+                unit=(u.hourangle, u.deg)).to_string(
+                'hmsdms', sep=':').split()
+        catalog.entries[name].add_quantity(
+            FASTSTARS.RA, ra, source=source)
+        catalog.entries[name].add_quantity(
+            FASTSTARS.DEC, dec, source=source)
+        catalog.entries[name].add_quantity(
+                FASTSTARS.VELOCITY, str(row['Vhel']), e_value=str(row['e_Vhel']), source=source)
+        catalog.entries[name].add_quantity(
+            FASTSTARS.PROPER_MOTION_RA, str(row['pmra']), e_value=str(row['e_pmra']), u_value='mas/yr', source=source)
+        catalog.entries[name].add_quantity(
+            FASTSTARS.PROPER_MOTION_DEC, str(row['pmdec']), e_value=str(row['e_pmdec']), u_value='mas/yr', source=source)
+        catalog.entries[name].add_quantity(
+            FASTSTARS.LUM_DIST, str(row['Dhel']), e_value=str(row['e_Dhel']), u_value='kpc', source=source)
+        catalog.entries[name].add_quantity(
+                FASTSTARS.CLAIMED_TYPE, 'pHVS', source=source)
+        catalog.entries[name].add_quantity(
+                FASTSTARS.SPECTRAL_TYPE, str(row['Type']), source=source)
+    catalog.journal_entries()
 
     return
