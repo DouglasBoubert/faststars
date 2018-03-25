e98194420a8094a11023b5f27785979b466350b4
guillochon <guillochon@gmail.com>
2018-03-24 18:12:07 -0700

    Update scripts, add gitignore, update tasks, repo names

--- scripts/auto-update-catalog.sh
+++ scripts/auto-update-catalog.sh
@@ -4,15 +4,15 @@ set -e
 PATH=/root/miniconda3/bin:/opt/local/bin:/usr/local/bin:$PATH ; export PATH
 LD_LIBRARY_PATH=/usr/local/lib:/opt/local/lib ; export LD_LIBRARY_PATH
 
-cd /var/www/html/sne/astrocats
-python -m astrocats supernovae git-pull
-python -m astrocats supernovae import
+cd /var/www/html/hvs/astrocats
+python -m astrocats faststars git-pull
+python -m astrocats faststars import
 SNEUPDATE=$?
 echo $SNEUPDATE
 if [[ $SNEUPDATE == 0 ]]; then
-	astrocats/supernovae/scripts/generate-web.sh
-	python -m astrocats supernovae git-pull
-	python -m astrocats supernovae git-push
+	astrocats/faststars/scripts/generate-web.sh
+	python -m astrocats faststars git-pull
+	python -m astrocats faststars git-push
 	#stamp=$(date +"%Y-%m-%d %k:%M")
 	#./commit-and-push-repos.sh "Auto update: $stamp"
 fi
--- scripts/generate-web.sh
+++ scripts/generate-web.sh
@@ -4,32 +4,32 @@ set -e
 PATH=/opt/local/bin:/usr/local/bin:$PATH ; export PATH
 LD_LIBRARY_PATH=/usr/local/lib:/opt/local/lib ; export LD_LIBRARY_PATH
 
-cd /var/www/html/sne/astrocats
-python -m astrocats.scripts.webcat -c sne &
+cd /var/www/html/hvs/astrocats
+python -m astrocats.scripts.webcat -c hvs &
 pids[0]=$!
-python -m astrocats.scripts.webcat -c sne -by &
+python -m astrocats.scripts.webcat -c hvs -by &
 pids[1]=$!
-python -m astrocats.supernovae.scripts.dupecat &
+python -m astrocats.faststars.scripts.dupecat &
 pids[2]=$!
-python -m astrocats.supernovae.scripts.conflictcat &
+python -m astrocats.faststars.scripts.conflictcat &
 pids[3]=$!
-python -m astrocats.scripts.bibliocat -c sne &
+python -m astrocats.scripts.bibliocat -c hvs &
 pids[4]=$!
-python -m astrocats.supernovae.scripts.erratacat &
+python -m astrocats.faststars.scripts.erratacat &
 pids[5]=$!
-python -m astrocats.scripts.hostcat -c sne &
-pids[6]=$!
-python -m astrocats.scripts.hammertime -c sne &
+#python -m astrocats.scripts.hostcat -c hvs &
+#pids[6]=$!
+python -m astrocats.scripts.hammertime -c hvs &
 pids[7]=$!
-python -m astrocats.supernovae.scripts.histograms &
-pids[8]=$!
-python -m astrocats.scripts.atelscbetsiaucs -c sne &
-pids[9]=$!
-python -m astrocats.supernovae.scripts.frbcat &
-pids[10]=$!
+#python -m astrocats.faststars.scripts.histograms &
+#pids[8]=$!
+#python -m astrocats.scripts.atelscbetsiaucs -c hvs &
+#pids[9]=$!
+#python -m astrocats.faststars.scripts.frbcat &
+#pids[10]=$!
 for pid in ${pids[*]}; do
 	wait $pid
 done
-cd /var/www/html/sne/astrocats/astrocats/supernovae/output/html
+cd /var/www/html/hvs/astrocats/astrocats/faststars/output/html
 bash thumbs.sh
-cd /var/www/html/sne/astrocats
+cd /var/www/html/hvs/astrocats
